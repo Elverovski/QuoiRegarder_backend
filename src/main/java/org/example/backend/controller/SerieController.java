e3c5627c0fc25e484ef8afb24a0bf567bff8bcb7
Mario Enrique Silva Benavides <2280534@collegemv.qc.ca>
2025-09-18 10:05:58 -0400

    adding CRUD functions

--- src/main/java/org/example/backend/controller/SerieController.java
+++ src/main/java/org/example/backend/controller/SerieController.java
@@ -1,4 +1,40 @@
 package org.example.backend.controller;
 
+import org.example.backend.models.Serie;
+import org.example.backend.service.SerieService;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/serie")
+@CrossOrigin
 public class SerieController {
+
+    public final SerieService service;
+
+
+    public SerieController(SerieService service) {
+        this.service = service;
+    }
+
+    @GetMapping("/getAllSeries")
+    public List<Serie> getAllSeries(){
+        return service.findAllSeries();
+    }
+
+    @PostMapping("/createSerie")
+    public Serie createSerie(@RequestBody Serie newSerie){
+        return service.createSerie(newSerie);
+    }
+
+    @PutMapping("/updateSerie/{id}")
+    public Serie updateSerie(@RequestBody Serie newOne, @PathVariable Long id){
+        return service.updateSerie(newOne, id);
+    }
+    @DeleteMapping("/deleteSerie/{id}")
+    public void deleteSerie(@PathVariable Long id){
+        service.deleteSerie(id);
+    }
 }
+
--- src/main/java/org/example/backend/service/SerieService.java
+++ src/main/java/org/example/backend/service/SerieService.java
@@ -1,7 +1,50 @@
 package org.example.backend.service;
 
+import org.example.backend.exception.ModelNotFoundException;
+import org.example.backend.models.Serie;
+import org.example.backend.repository.SerieRepository;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class SerieService {
+    private final SerieRepository serieRepository;
+
+    public SerieService(SerieRepository serieRepository) {
+        this.serieRepository = serieRepository;
+    }
+
+    // GET
+    public List<Serie> findAllSeries() {
+        return serieRepository.findAll();
+    }
+
+    public List<Serie> findSeriesByName(String titre) {
+        return serieRepository.findSerieByTitre(titre);
+    }
+
+    // Update
+    public Serie updateSerie(Serie newOne, Long id) {
+        return serieRepository.findById(id)
+                .map(serie -> {
+                    serie.setTitre(serie.getTitre());
+                    serie.setGenre(serie.getGenre());
+                    serie.setNbepisodes(serie.getNbepisodes());
+                    serie.setNote(serie.getNote());
+                    return serieRepository.save(serie);
+                }).orElseThrow(() -> new ModelNotFoundException(id, "Serie"));
+    }
+
+    // Delete
+    public void deleteSerie(Long id) {
+        serieRepository.deleteById(id);
+    }
+
+    // Create
+    public Serie createSerie(Serie newSerie) {
+        return serieRepository.save(newSerie);
+    }
+
+
 }
