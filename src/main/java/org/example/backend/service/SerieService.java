2ea523a8e8959be575b1d3f7a97954347890bab0
Papemomath <papemomathdiba@gmail.com>
2025-09-19 20:02:36 -0400

    ajout des seach-serie

--- src/main/java/org/example/backend/service/SerieService.java
+++ src/main/java/org/example/backend/service/SerieService.java
@@ -20,7 +20,7 @@ public class SerieService {
         return serieRepository.findAll();
     }
 
-    public List<Serie> findSeriesByName(String titre) {
+    public Serie findSeriesByName(String titre) {
         return serieRepository.findSerieByTitre(titre);
     }
 
@@ -46,5 +46,17 @@ public class SerieService {
         return serieRepository.save(newSerie);
     }
 
+
+    public List<Serie> searchSerie(String genre, int nbEpisodes){
+        return serieRepository.findSerieByGenreAndNbepisodes(genre, nbEpisodes);
+    }
+
+    public List<Serie> searchSerie(String genre) {
+        return serieRepository.findSerieByGenre(genre);
+    }
+
+    public List<Serie> searchSerie(int nbepisodes) {
+        return serieRepository.findSerieByNbepisodesGreaterThanEqual(nbepisodes);
+    }
 
 }
