1b94228fb56c8a2ea2c5e4bda9462d7ea910a5a0
Papemomath <papemomathdiba@gmail.com>
2025-10-02 21:00:45 -0400

    ajout champ password + data pour episode

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,35 +1,34 @@
 
 -- User data
-insert into users (id, nom, prenom, email, genre) values (1, 'Stainer', 'Peirce', 'pstainer0@earthlink.net', 'Male');
-insert into users (id, nom, prenom, email, genre) values (2, 'Lazer', 'Fifine', 'flazer1@google.co.jp', 'Female');
-insert into users (id, nom, prenom, email, genre) values (3, 'Hanley', 'Aguie', 'ahanley2@yandex.ru', 'Male');
-insert into users (id, nom, prenom, email, genre) values (4, 'Tenney', 'Elle', 'etenney3@nsw.gov.au', 'Female');
-insert into users (id, nom, prenom, email, genre) values (5, 'McKeachie', 'Arty', 'amckeachie4@feedburner.com', 'Male');
-insert into users (id, nom, prenom, email, genre) values (6, 'Spurr', 'Marc', 'mspurr5@smh.com.au', 'Male');
-insert into users (id, nom, prenom, email, genre) values (7, 'Jewess', 'Eddy', 'ejewess6@sciencedirect.com', 'Female');
-insert into users (id, nom, prenom, email, genre) values (8, 'Scrimshaw', 'Merrie', 'mscrimshaw7@creativecommons.org', 'Female');
-insert into users (id, nom, prenom, email, genre) values (9, 'Broom', 'Wallie', 'wbroom8@buzzfeed.com', 'Male');
-insert into users (id, nom, prenom, email, genre) values (10, 'Alben', 'Dallis', 'dalben9@odnoklassniki.ru', 'Male');
-insert into users (id, nom, prenom, email, genre) values (11, 'Thyng', 'Lek', 'lthynga@macromedia.com', 'Male');
-insert into users (id, nom, prenom, email, genre) values (12, 'Wyse', 'Salim', 'swyseb@goo.gl', 'Male');
-insert into users (id, nom, prenom, email, genre) values (13, 'Wapol', 'Eamon', 'ewapolc@github.io', 'Male');
-insert into users (id, nom, prenom, email, genre) values (14, 'Agar', 'Lanae', 'lagard@jimdo.com', 'Female');
-insert into users (id, nom, prenom, email, genre) values (15, 'Moine', 'Georgiana', 'gmoinee@cdc.gov', 'Female');
-insert into users (id, nom, prenom, email, genre) values (16, 'Levine', 'Holt', 'hlevinef@pinterest.com', 'Male');
-insert into users (id, nom, prenom, email, genre) values (17, 'Markovich', 'Ali', 'amarkovichg@delicious.com', 'Male');
-insert into users (id, nom, prenom, email, genre) values (18, 'Squeers', 'Corbie', 'csqueersh@chron.com', 'Male');
-insert into users (id, nom, prenom, email, genre) values (19, 'Crathern', 'Em', 'ecratherni@gizmodo.com', 'Female');
-insert into users (id, nom, prenom, email, genre) values (20, 'Hallock', 'Retha', 'rhallockj@com.com', 'Female');
-insert into users (id, nom, prenom, email, genre) values (21, 'Scamal', 'Evie', 'escamalk@naver.com', 'Female');
-insert into users (id, nom, prenom, email, genre) values (22, 'Crickmore', 'Amandi', 'acrickmorel@mit.edu', 'Female');
-insert into users (id, nom, prenom, email, genre) values (23, 'Bleackley', 'Hinze', 'hbleackleym@merriam-webster.com', 'Male');
-insert into users (id, nom, prenom, email, genre) values (24, 'Fairham', 'Charles', 'cfairhamn@slate.com', 'Male');
-insert into users (id, nom, prenom, email, genre) values (25, 'Roizn', 'Aurel', 'aroizno@msn.com', 'Female');
-insert into users (id, nom, prenom, email, genre) values (26, 'Perl', 'Ernesta', 'eperlp@smugmug.com', 'Female');
-insert into users (id, nom, prenom, email, genre) values (27, 'Ashelford', 'Meghann', 'mashelfordq@nationalgeographic.com', 'Female');
-insert into users (id, nom, prenom, email, genre) values (28, 'Tremblot', 'Jasen', 'jtremblotr@senate.gov', 'Male');
-insert into users (id, nom, prenom, email, genre) values (29, 'Burgess', 'Drusie', 'dburgesss@cam.ac.uk', 'Female');
-insert into users (id, nom, prenom, email, genre) values (30, 'McGourty', 'Nicolina', 'nmcgourtyt@google.co.uk', 'Female');
+insert into users (id, nom, prenom, email, genre, password) values (2, 'Lazer', 'Fifine', 'lazer@example.com', 'Female', 'Lazer');
+insert into users (id, nom, prenom, email, genre, password) values (3, 'Hanley', 'Aguie', 'hanley@example.com', 'Male', 'Hanley');
+insert into users (id, nom, prenom, email, genre, password) values (4, 'Tenney', 'Elle', 'tenney@example.com', 'Female', 'Tenney');
+insert into users (id, nom, prenom, email, genre, password) values (5, 'McKeachie', 'Arty', 'mckeachie@example.com', 'Male', 'McKeachie');
+insert into users (id, nom, prenom, email, genre, password) values (6, 'Spurr', 'Marc', 'spurr@example.com', 'Male', 'Spurr');
+insert into users (id, nom, prenom, email, genre, password) values (7, 'Jewess', 'Eddy', 'jewess@example.com', 'Female', 'Jewess');
+insert into users (id, nom, prenom, email, genre, password) values (8, 'Scrimshaw', 'Merrie', 'scrimshaw@example.com', 'Female', 'Scrimshaw');
+insert into users (id, nom, prenom, email, genre, password) values (9, 'Broom', 'Wallie', 'broom@example.com', 'Male', 'Broom');
+insert into users (id, nom, prenom, email, genre, password) values (10, 'Alben', 'Dallis', 'alben@example.com', 'Male', 'Alben');
+insert into users (id, nom, prenom, email, genre, password) values (11, 'Thyng', 'Lek', 'thyng@example.com', 'Male', 'Thyng');
+insert into users (id, nom, prenom, email, genre, password) values (12, 'Wyse', 'Salim', 'wyse@example.com', 'Male', 'Wyse');
+insert into users (id, nom, prenom, email, genre, password) values (13, 'Wapol', 'Eamon', 'wapol@example.com', 'Male', 'Wapol');
+insert into users (id, nom, prenom, email, genre, password) values (14, 'Agar', 'Lanae', 'agar@example.com', 'Female', 'Agar');
+insert into users (id, nom, prenom, email, genre, password) values (15, 'Moine', 'Georgiana', 'moine@example.com', 'Female', 'Moine');
+insert into users (id, nom, prenom, email, genre, password) values (16, 'Levine', 'Holt', 'levine@example.com', 'Male', 'Levine');
+insert into users (id, nom, prenom, email, genre, password) values (17, 'Markovich', 'Ali', 'markovich@example.com', 'Male', 'Markovich');
+insert into users (id, nom, prenom, email, genre, password) values (18, 'Squeers', 'Corbie', 'squeers@example.com', 'Male', 'Squeers');
+insert into users (id, nom, prenom, email, genre, password) values (19, 'Crathern', 'Em', 'crathern@example.com', 'Female', 'Crathern');
+insert into users (id, nom, prenom, email, genre, password) values (20, 'Hallock', 'Retha', 'hallock@example.com', 'Female', 'Hallock');
+insert into users (id, nom, prenom, email, genre, password) values (21, 'Scamal', 'Evie', 'scamal@example.com', 'Female', 'Scamal');
+insert into users (id, nom, prenom, email, genre, password) values (22, 'Crickmore', 'Amandi', 'crickmore@example.com', 'Female', 'Crickmore');
+insert into users (id, nom, prenom, email, genre, password) values (23, 'Bleackley', 'Hinze', 'bleackley@example.com', 'Male', 'Bleackley');
+insert into users (id, nom, prenom, email, genre, password) values (24, 'Fairham', 'Charles', 'fairham@example.com', 'Male', 'Fairham');
+insert into users (id, nom, prenom, email, genre, password) values (25, 'Roizn', 'Aurel', 'roizn@example.com', 'Female', 'Roizn');
+insert into users (id, nom, prenom, email, genre, password) values (26, 'Perl', 'Ernesta', 'perl@example.com', 'Female', 'Perl');
+insert into users (id, nom, prenom, email, genre, password) values (27, 'Ashelford', 'Meghann', 'ashelford@example.com', 'Female', 'Ashelford');
+insert into users (id, nom, prenom, email, genre, password) values (28, 'Tremblot', 'Jasen', 'tremblot@example.com', 'Male', 'Tremblot');
+insert into users (id, nom, prenom, email, genre, password) values (29, 'Burgess', 'Drusie', 'burgess@example.com', 'Female', 'Burgess');
+insert into users (id, nom, prenom, email, genre, password) values (30, 'McGourty', 'Nicolina', 'mcgourty@example.com', 'Female', 'McGourty');
 
 --------------------------------------------------------------------------------------------
 -- Serie Data
